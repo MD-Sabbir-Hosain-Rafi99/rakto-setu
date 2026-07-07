262cd9efda551d5879c52193ec152103d01832c4
MD Sabbir Hosain Rafi <mdsabbirhosainrafi@gmail.com>
2026-07-07 06:23:38 +0600

    Configure API URL for production

--- server/server.js
+++ server/server.js
@@ -20,11 +20,15 @@ import telegramRoutes from "./routes/telegramRoutes.js";
 
 dotenv.config({ path: "./.env" });
 
-console.log("MONGO URI:", process.env.MONGO_URI);
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
